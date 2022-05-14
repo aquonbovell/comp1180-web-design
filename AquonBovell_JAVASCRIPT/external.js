cfe5c83d606d5f97fb63cb5b9b73c95f8405c54a
Aquon Bovell <aquon_bovell@outlook.com>
2022-05-13 20:26:48 -0400

    commit

--- AquonBovell_JAVASCRIPT/external.js
+++
@@ -1,5 +0,0 @@
-function times() {
-    var num1 = prompt("Enter the first number: ");
-    var num2 = prompt("Enter the second number: ");
-    document.write("The multiple is "+ (num1 * num2));
-}
