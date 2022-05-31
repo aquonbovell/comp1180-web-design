ea7fcc9a2273949f4f986d07b5a448c2974aa0e2
Aquon Bovell <aquon_bovell@outlook.com>
2022-05-31 13:04:07 -0400

    done

--- individual-project/js/app.js
+++ individual-project/js/app.js
@@ -1,6 +1,7 @@
 const questions = document.querySelectorAll(".question");
 const navlist = document.querySelector(".nav-list");
 const menuBtn = document.querySelector(".menu-btn");
+const latestnewsDOM = document.querySelector('.latest-news');
 let menuOpen = false;
 
 menuBtn.addEventListener("click", () => {
@@ -25,5 +26,51 @@ questions.forEach(function (question) {
       }
     });
     question.classList.toggle("show-text");
+  });
+});
+
+class Products {
+  async getProducts() {
+      try {
+          let result = await fetch("news.json");
+          let data = await result.json();
+          let products = data.items;
+          products = products.map(item => {
+              const { title, link} = item.fields;
+              const {day, month, year} = item.fields.date;
+              return { title, link, day, month,year};
+          });
+          return products;
+      } catch (error) {
+          console.log(error);
+      }
+  }
+}
+
+class UI {
+  displayProduct(product) {
+    let result = '';
+    product.forEach(product => {
+      result += `<li>
+                    <h3>
+                      <a href="${product.link}">
+                        ${product.title}
+                      </a>
+                    </h3>
+                    <div>
+                      <span>${product.month}</span> <span>${product.day}</span><span>${product.year}</span>
+                    </div>
+                  </li>`;
+      console.log(product.day);
+    });
+    latestnewsDOM.innerHTML = result;
+  }
+}
+document.addEventListener("DOMContentLoaded", () => {
+  const products = new Products();
+  const ui = new UI();
+  //get all products
+  products.getProducts().then(products => {
+    ui.displayProduct(products);
   });
 });
